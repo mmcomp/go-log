1a523f58c88f2613ce12580380947ae239f43028
Mehrdad <m.mirsamie@gmail.com>
2021-07-13 20:24:05 +0430

    add doc for package

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Package log is a personal logger that was created with Go Lang and has different features such as
+- returning running function name
+- ability to add unlimited amount of Prefix
+- different colors for different types of log such as Error and Highlight and etc
+- special global method as Begin which generates a logger instance and also with End together measures the time elapsed
+*/
+package log
